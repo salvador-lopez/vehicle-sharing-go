120d0dfb1a1c6a0411991dddd34b0b96ce435492
slopezreyes <s.lopezreyes255@gmail.com>
2023-12-20 11:32:49 +0100

    Remove unused code from domain event consumer binary

--- cmd/inventory-vehicles/domain-event-consumer/cmd/consumer.go
+++ cmd/inventory-vehicles/domain-event-consumer/cmd/consumer.go
@@ -7,14 +7,12 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"reflect"
 	"sync"
 	"syscall"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/google/uuid"
-	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
 
 	"vehicle-sharing-go/internal/inventory/vehicle/database/gorm"
@@ -158,44 +156,4 @@ func (v vinDecoderFake) Decode(_ context.Context, vinNumber string) (*projection
 		AssemblyPlant: &assemblyPlant,
 		SN:            &sn,
 	}, nil
-}
-
-func toTimeHookFunc() mapstructure.DecodeHookFunc {
-	return func(
-		f reflect.Type,
-		t reflect.Type,
-		data interface{}) (interface{}, error) {
-		if t != reflect.TypeOf(time.Time{}) {
-			return data, nil
-		}
-
-		switch f.Kind() {
-		case reflect.String:
-			return time.Parse(time.RFC3339, data.(string))
-		case reflect.Float64:
-			return time.Unix(0, int64(data.(float64))*int64(time.Millisecond)), nil
-		case reflect.Int64:
-			return time.Unix(0, data.(int64)*int64(time.Millisecond)), nil
-		default:
-			return data, nil
-		}
-	}
-}
-
-func Decode(input any, result interface{}) error {
-	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-		TagName:  "json",
-		Metadata: nil,
-		DecodeHook: mapstructure.ComposeDecodeHookFunc(
-			toTimeHookFunc()),
-		Result: result,
-	})
-	if err != nil {
-		return err
-	}
-
-	if err := decoder.Decode(input); err != nil {
-		return err
-	}
-	return err
 }
