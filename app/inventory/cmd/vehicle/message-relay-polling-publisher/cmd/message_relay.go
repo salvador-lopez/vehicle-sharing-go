7ffcde81ee85e33fa402a5e74b4fec5d004e7817
salvador-lopez <s.lopezreyes255@gmail.com>
2025-06-17 16:08:34 +0200

    message-relay-polling-publisher cmd

--- app/inventory/cmd/vehicle/message-relay-polling-publisher/cmd/message_relay.go
+++ app/inventory/cmd/vehicle/message-relay-polling-publisher/cmd/message_relay.go
@@ -1,6 +1,19 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+	gormpkg "vehicle-sharing-go/pkg/database/gorm"
+	"vehicle-sharing-go/pkg/messaging"
+	kafkapkg "vehicle-sharing-go/pkg/messaging/kafka"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -8,7 +21,7 @@ import (
 type KafkaConfig struct {
 	Brokers []string
 	GroupId string
-	Topics  []string
+	Topic   string
 }
 
 type DbConfig struct {
@@ -26,8 +39,86 @@ type DbConn struct {
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "Run the application",
+	Short: "Run the Message Relay Polling Publisher",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		errc := make(chan error)
+
+		// Graceful shutdown
+		go func() {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+			errc <- fmt.Errorf("%s", <-c)
+		}()
+
+		logger := log.New(os.Stderr, "[inventory-vehicles-message-relay-polling-publisher] ", log.Ltime)
+
+		var dbCfg DbConfig
+		err := viper.UnmarshalKey("db", &dbCfg)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		connCfg := dbCfg.Conn
+		dbConn, err := gormpkg.NewConnectionFromConfig(&gormpkg.Config{
+			Host:         connCfg.Host,
+			Port:         connCfg.Port,
+			UserName:     connCfg.User,
+			Password:     connCfg.Password,
+			DatabaseName: dbCfg.Name,
+			Logger:       logger,
+			LogQueries:   false,
+		})
+		if err != nil {
+			logger.Fatalf("failed to create db connection: %v", err)
+		}
+
+		outboxRepo := gormpkg.NewOutboxRepository(dbConn)
+
+		// Load config
+		var kafkaCfg KafkaConfig
+		err = viper.UnmarshalKey("kafka", &kafkaCfg)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		producer, err := kafka.NewProducer(&kafka.ConfigMap{
+			"bootstrap.servers":     strings.Join(kafkaCfg.Brokers, ","),
+			"broker.address.family": "v4",
+			"group.id":              kafkaCfg.GroupId,
+			"auto.offset.reset":     "earliest",
+		})
+		if err != nil {
+			logger.Fatalf("failed to create kafka producer: %v", err)
+		}
+		defer producer.Close()
+
+		eventPublisher := kafkapkg.NewEventPublisher(producer, kafkaCfg.Topic)
+
+		// Poll loop
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
+
+		p := messaging.NewPollingPublisher(
+			outboxRepo,
+			eventPublisher,
+			2*time.Second,
+			10,
+			logger,
+		)
+
+		go func() {
+			if err := p.Run(ctx); err != nil {
+				errc <- err
+			}
+		}()
+
+		logger.Println("Massage Relay Polling Publisher started successfully")
+
+		// Wait for error signal.
+		logger.Printf("exiting (%v)", <-errc)
+
 		return nil
 	},
 }
